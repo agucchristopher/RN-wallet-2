0b9011af77dcaac5267f1a17cba1e074f804a8c6
Agu Christopher <aguchris740@gmail.com>
2023-10-11 00:19:55 +0100

    Init expo app

--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,19 @@
 import { Stack } from "expo-router";
-
+import { useFonts } from "expo-font";
+import { ActivityIndicator } from "react-native";
 export default () => {
+  const [fontsLoaded] = useFonts({
+    RBold: require("../assets/fonts/Raleway-Bold.ttf"),
+    RMedium: require("../assets/fonts/Raleway-Medium.ttf"),
+    RRegular: require("../assets/fonts/Raleway-Regular.ttf"),
+  });
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
   return (
     <Stack
       screenOptions={{
-        statusBarColor: "dodgerblue",
+        statusBarColor: "#121212",
       }}
     >
       <Stack.Screen name="index" options={{ headerShown: false }} />
--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { FlatList } from "react-native-gesture-handler";
 import { Image } from "react-native";
@@ -6,13 +6,59 @@ import { Image } from "react-native";
 const app = () => {
   return (
     <View style={styles.container}>
-      <FlatList
-        data={["", "", "", ""]}
-        renderItem={({ item, index }) => {
-          <Image source={require(`../assets/males/m-${index}`)} />;
+      <Text
+        style={{
+          fontSize: 30,
+          color: "white",
+          fontFamily: "RRegular",
+          display: "flex",
+          alignSelf: "center",
+          margin: 30,
         }}
-      />
-      <Text>app</Text>
+      >
+        My Wallet
+      </Text>
+      <View
+        style={{
+          alignSelf: "center",
+          margin: 15,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <FlatList
+          horizontal={true}
+          data={["on-boarding-1.png", "on-boarding-2.png", "on-boarding-3.png"]}
+          renderItem={({ item, index }) => {
+            let image = `../assets/images/` + item;
+            // console.log(item);
+            return (
+              <View
+                style={{
+                  height: 350,
+                  width: 250,
+                  backgroundColor: "#1d1d1d",
+                  margin: 15,
+                  alignContent: "flex-start",
+                  borderRadius: 19,
+                  marginLeft: index === 0 ? 15 : 10,
+                  justifyContent: "center",
+                  opacity: 0.9,
+                }}
+              >
+                <Image
+                  source={require(`../assets/images/on-boarding-1.png`)}
+                  style={{
+                    height: 250,
+                    width: Dimensions.get("window").width * 0.6,
+                  }}
+                  resizeMode="contain"
+                />
+              </View>
+            );
+          }}
+        />
+      </View>
     </View>
   );
 };
@@ -21,7 +67,7 @@ export default app;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "dodgerblue",
+    backgroundColor: "#121212",
     flex: 1,
   },
 });
